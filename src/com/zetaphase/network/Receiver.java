ac74dcb8660499b0c6c6275726fdf1da74cc1064
daveho8888 <daveho8888@gmail.com>
2016-08-27 11:43:42 -0700

    initialized Receiver.java class

---
+++ src/com/zetaphase/network/Receiver.java
@@ -0,0 +1,16 @@
+package com.zetaphase.network;
+
+public class Receiver extends Thread {
+	
+	PortManager portManager;
+	
+	public Receiver(PortManager portManager){
+		super();
+		this.portManager = portManager;
+	}
+	
+	@Override
+	public void run(){
+		
+	}
+}
